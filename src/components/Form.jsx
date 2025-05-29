5b969065f01b75a85bb5b779b2148abdcaa76632
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-05-30 02:00:25 +0530

    worked on the PDF

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,33 +6,67 @@ import { Resume } from './Resume';
 export const Form = () => {
 
     const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+    const [about, setAbout] = useState();
+    const [experience, setExperience] = useState();
 
     const nameChangeHandler = (e) => {
         setName(e.target.value);
-        console.log(name);
+    }
+
+    const emailChangeHandler = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const phoneChangeHandler = (e) => {
+        setPhone(e.target.value);
+    }
+
+    const aboutChangeHandler = (e) => {
+        setAbout(e.target.value);
+        console.log(about);
+    }
+
+    const experienceChangeHandler = (e) => {
+        setExperience(e.target.value);
+        console.log(experience);
     }
 
   return (
     <>
-    <div className='mx-auto my-12 w-fit bg-white p-6 rounded-2xl shadow-xl'>
+    <div className='mx-auto my-12 w-fit bg-white p-6 rounded-2xl shadow-xl min-w-4/12'>
         <form className='flex flex-col gap-3 text-center'>
             <span className='text-xl mb-3'>Enter your details</span>
-            <span>
+            <span className='flex flex-row gap-3 justify-around items-center'>
                 <label>Name: </label>
-                <input onChange={nameChangeHandler} className='border-1 rounded p-1' type='text'/>
+                <input onChange={nameChangeHandler} className='border-1 rounded p-1 w-xs' type='text'/>
             </span>
-            <span>
+            <span className='flex flex-row gap-3 justify-around items-center'>
                 <label>Email: </label>
-                <input className='border-1 rounded p-1' type='text'/>
+                <input onChange={emailChangeHandler} className='border-1 rounded p-1 w-xs' type='text'/>
             </span>
-            <span>
+            <span className='flex flex-row gap-3 justify-around items-center'>
                 <label>Phone: </label>
-                <input className='border-1 rounded p-1' type='text'/>
+                <input onChange={phoneChangeHandler} className='border-1 rounded p-1 w-xs' type='text'/>
+            </span>
+            <span className='flex flex-row gap-3 items-center'>
+                <label>About me: </label>
+                <textarea onChange={aboutChangeHandler} className='border-1 rounded p-1 w-xs' type='text'/>
+            </span>
+            <span className='flex flex-row gap-3 justify-around items-center'>
+                <label>Experience: </label>
+                <input className='border-1 rounded p-1 w-xs' value={experience} type='number' onChange={experienceChangeHandler}/>
+            </span>
+            <span className='flex flex-col gap-3'>
+                    {Array.from({ length: experience }).map((_,index) => (
+                        <input type='text' className='border-1 rounded p-1 w-full' placeholder={`Experience ${index+1}`} />
+                    ))}
             </span>
         </form>
     </div>
-    <PDFViewer>
-        <Resume useName={name}/>
+    <PDFViewer width="100%" height="100%">
+        <Resume useName={name} usePhone={phone} useEmail={email} useAbout={about} />
     </PDFViewer>
     </>
   )
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,57 @@
-import { Document, Page, Text } from '@react-pdf/renderer'
+import { Document, Page, Text, StyleSheet, View } from '@react-pdf/renderer'
 import React from 'react'
 
-export const Resume = ({useName}) => {
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'column',
+    gap: 3
+  },
+  top: {
+    marginLeft: 30,
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 24
+  },
+  section: {
+    marginLeft: 30,
+  },
+  normal: {
+    fontSize: 12,
+    marginRight: 30,
+  },
+  normalHeadings: {
+    fontSize: 12,
+    padding: 4,
+    marginRight: 30,
+    color: 'white',
+  },
+  headingStyle: {
+    marginLeft: 30,
+    marginRight: 30,
+    backgroundColor: 'rgb(50, 168, 82)',
+    borderRadius: 5,
+  }
+})
+
+export const Resume = ({useName, useEmail, usePhone, useAbout}) => {
   return (
     <Document>
-        <Page>
-            <Text>{useName}</Text>
+        <Page style={styles.page}>
+          <View style={styles.top}>
+            <Text style={styles.title}>{useName}</Text>
+            <Text style={styles.normal}>{useEmail}</Text>
+            <Text style={styles.normal}>{usePhone}</Text>
+          </View>
+          <View style={styles.headingStyle}>
+            <Text style={styles.normalHeadings}>About me</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.normal}>{useAbout}</Text>
+          </View>
+          <View style={styles.headingStyle}>
+            <Text style={styles.normalHeadings}>Experience</Text>
+          </View>
         </Page>
     </Document>
   )
