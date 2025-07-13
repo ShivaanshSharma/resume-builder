f07720b1269fb73d46765d50a9fcdb1510ef0367
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-07-13 20:24:12 +0530

    some minor UI changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export const Home = ({onChange}) => {
   return (
     <div className='flex flex-col h-120 lg:h-130 justify-around'>
       <span className='flex flex-col mx-12'>
-        <span className='text-2xl lg:text-6xl md:text-4xl font-medium text-white leading-tight'>Your story deserves a spotlight...<br />Let your resume speak before you do.</span>
+        <span className='text-4xl lg:text-5xl md:text-4xl font-medium text-white leading-tight'>Your story deserves a spotlight...<br />Let your resume speak before you do.</span>
         <Button onClick={onChange} text="Create your resume now!" />
       </span>
     </div>
